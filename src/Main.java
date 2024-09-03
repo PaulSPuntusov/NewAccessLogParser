251fa3461bb327d51fe9c330d878d523ce0e31e4
PaulSPuntusov <pps_ru@mail.ru>
2024-09-03 20:59:45 +0300

    добавил обработку исключений

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main  {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Введите путь до файла и нажмите <Enter>: ");
         String path = new Scanner(System.in).nextLine();
@@ -21,37 +19,35 @@ public class Main {
 
         }
         if ((fileExists == true) && (isDirectory == false)) {
+            BufferedReader reader = null;
             try {
                 FileReader fileReader = new FileReader(path);
-                BufferedReader reader = new BufferedReader(fileReader);
-                String line;
-                int count = 0;
-                int length = 0;
-                int maxLength = 0;
-                int minLength = 0;
-                if (reader.readLine() != null) minLength = reader.readLine().length();
-                while ((line = reader.readLine()) != null) {
+                reader = new BufferedReader(fileReader);
+            } catch (FileNotFoundException ffe) {
+                System.out.println("FileNotFound");
+            }
+            String line;
+            int count = 0;
+            int length = 0;
+            int maxLength = 0;
+            int minLength = 0;
+            if (reader.readLine() != null) minLength = reader.readLine().length();
+            while ((line = reader.readLine()) != null) {
+                length = line.length();
+                if (length > 1024) {
+                    throw new RuntimeException("Строка не должна быть длиннее 1024 символа");
+                }
+                try {
                     length = line.length();
-                    if (length > 1024){
-                        throw new RuntimeException("Строка не должна быть длиннее 1024 символа");
-                    }
-                    try {
-                        length = line.length();
-                    } catch (RuntimeException re){
-                        try {
-                            System.out.println( "String must be shorter than 1024 characters");
-                        } catch (MyOwnException moe){
-                            System.out.println("обернул исключение на длину 1024 своим кастомным исключением");
-                    }
                     if (length > maxLength) maxLength = length;
                     if (length < minLength) minLength = length;
                     count++;
+                } catch (RuntimeException re) {
+                        System.out.println("String must be shorter than 1024 characters");
                 }
-                System.out.println( "Количество строка: "+ count);
-                System.out.println("Самая длинная строка: "+maxLength);
-                System.out.println("Самая короткая строка: "+minLength);
-            } catch (Exception ex) {
-                ex.printStackTrace();
+                System.out.println("Количество строка: " + count);
+                System.out.println("Самая длинная строка: " + maxLength);
+                System.out.println("Самая короткая строка: " + minLength);
             }
 
         }
