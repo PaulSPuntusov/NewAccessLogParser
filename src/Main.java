73457b3eb0aac32ae81d9ae4b2c0d36044c1fdd7
PaulSPuntusov <pps_ru@mail.ru>
2024-09-03 22:11:41 +0300

    я считаю по Мозилле

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Main  {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Введите путь до файла и нажмите <Enter>: ");
-        String path = new Scanner(System.in).nextLine();
-        // path = "C:\\Users\\pps_r\\IdeaProjects\\AccessLogParser\\log.txt";
+        //System.out.println("Введите путь до файла и нажмите <Enter>: ");
+        //String path = new Scanner(System.in).nextLine();
+        String path = "C:\\Users\\pps_r\\IdeaProjects\\AccessLogParser\\access.log";
         File file = new File(path);
         boolean fileExists = file.exists();
         boolean isDirectory = file.isDirectory();
@@ -28,27 +28,44 @@ public class Main  {
             }
             String line;
             int count = 0;
+            int countMozilla = 0;
+            int countGooglebot = 0;
+            int countYandexbot = 0;
             int length = 0;
-            int maxLength = 0;
-            int minLength = 0;
-            if (reader.readLine() != null) minLength = reader.readLine().length();
             while ((line = reader.readLine()) != null) {
+                /*
                 length = line.length();
+                String[] parts = firstBrackets.split(";");
+                if (parts.length >=2){
+                    String fragment = parts[1];
+                }
+                 */
+                if (line.matches(".*"+"Mozilla"+".*")){
+                    countMozilla++;
+                }
+                if (line.matches(".*"+"YandexBot"+".*")){
+                    countYandexbot++;
+                }
+                if (line.matches(".*"+"Googlebot"+".*")){
+                    countGooglebot++;
+                }
                 if (length > 1024) {
                     throw new RuntimeException("Строка не должна быть длиннее 1024 символа");
                 }
                 try {
                     length = line.length();
-                    if (length > maxLength) maxLength = length;
-                    if (length < minLength) minLength = length;
                     count++;
                 } catch (RuntimeException re) {
                         System.out.println("String must be shorter than 1024 characters");
                 }
-                System.out.println("Количество строка: " + count);
-                System.out.println("Самая длинная строка: " + maxLength);
-                System.out.println("Самая короткая строка: " + minLength);
+
             }
+            System.out.println("Количество строка: " + count);
+            System.out.println("Число вхождений Mozilla:"+ countMozilla);
+            System.out.println("Число вхождений Googlebot:"+ countGooglebot);
+            System.out.println("Число вхождений Yandexbot:"+ countYandexbot);
+            System.out.println("Доля запросов от Гуглоботов:" + (double) countGooglebot/countMozilla);
+            System.out.println("Доля запросов от Яндексботов:" + (double) countYandexbot/countMozilla);
 
         }
     }
